9ef5778d345b2ceebbdbbc65e932f7cd3d3eaa77
Caleb Klinger <82426784+TrippingLettuce@users.noreply.github.com>
2023-01-16 21:17:41 -0700

    Update bot.py

--- Installation and Prefix Commands/bot.py
+++ Installation and Prefix Commands/bot.py
@@ -1,11 +1,6 @@
 import discord
 from discord.ext import commands
 
-#---Other Imports
-import tok
-
-TOKEN = tok.token
-
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
@@ -17,4 +12,4 @@ async def on_ready():
     print(f'Logged in as {bot.user}') #Bot Name
     print(bot.user.id) #Bot ID
 
-bot.run(TOKEN)
+bot.run('TOKEN') #Inster Token ID here should look something like MTA2NDc0NTE3OTAxMTEwODk4Ng.G32ofv...
